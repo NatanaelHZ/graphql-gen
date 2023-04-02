34465e9a7073147189c05980d42c7d1c4ca177ad
NatanaelHZ <natanael_henrik28@outlook.com>
2023-04-02 17:21:31 -0300

    Criado exemplos de querys

--- src/GraphQLSchemaSyntax.hs
+++ src/GraphQLSchemaSyntax.hs
@@ -17,8 +17,31 @@ data Type = Name String
           deriving Show
 
 
+-- Examples:
+{- Examples generic -}
 schema1 = Type "Query" 
             [
                 (Field "artist" [Arg "id" (Name "ID")] (Name "Artist")),
                 (Field "movie" [Arg "id" (Name "ID")] (Name "Movie"))
             ]
+
+{- Examples Space-X API -}
+-- Equivalent: type Query
+spacexSchema = Type "Query"
+                [
+                    -- Equivalent: capsule(id: ID!): Capsule
+                    --(Field "capsule" [Arg "id" (Name "ID")]  (Name "Capsule")),
+
+                    -- capsules(find: CapsulesFind, limit: Int, offset: Int, order: String, sort: String): [Capsule]
+                    (Field "capsules" [
+                                Arg "find" (Name "CapsulesFind"),
+                                Arg "limit" (Name "Int"),
+                                Arg "offset" (Name "Int"),
+                                Arg "order" (Name "String"),
+                                Arg "sort" (Name "String")
+                            ]
+                            -- Return
+                            (TypeList [(Name "Capsule")])),
+                    -- company: Info
+                    (Field "company" [] (Name "Info"))
+                ]
